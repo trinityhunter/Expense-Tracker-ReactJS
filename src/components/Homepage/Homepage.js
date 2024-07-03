4ba63d832c6fbe0bddb7053e3f6c48c915d52549
Raviraj Bhosale <ravirajbhosale@Ravirajs-MacBook-Air.local>
2024-07-03 20:49:44 +0530

    assests path updated

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Homepage.css';
-import piechart from "/Users/ravirajbhosale/Desktop/React/expense_tracker/src/assests/my expenses analysis.PNG"
-import barchart from "/Users/ravirajbhosale/Desktop/React/expense_tracker/src/assests/th-3.jpeg"
+import piechart from "../../assests/my-expenses-analysis.jpeg"
+import barchart from "../../assests/th-3.jpeg"
 import { useNavigate } from 'react-router-dom';
 
 const Homepage = () => {
